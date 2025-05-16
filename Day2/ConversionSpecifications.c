c6f135806fd5b1f31fcd40b3fb71193bd2442554
junhong <junhongwang31@gmail.com>
2025-05-16 18:22:05 -0400

    small changes i guess

---
+++ Day2/ConversionSpecifications.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    int i = 10;
+    int j = 128;
+    float x = 1.128f;
+    float y = 3838.1840;
+    // printf("%d %d\n", i); //some compilers may not catch this but it looks like gcc does
+    // printf("%d\n", i, j); //gcc will also catch this, but other compilers may notfasdfasd
+}
